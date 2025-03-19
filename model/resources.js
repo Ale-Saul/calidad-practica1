0522943ebc2e2242e7d4925a2f0e8d78dfc92112
Alejandro <alejandro.mont.cesp@gmail.com>
2025-03-18 22:25:11 -0400

    Se soluciono el smell de la funcion action del numero de variables de la funcion

---
+++ model/resources.js
@@ -0,0 +1,8 @@
+define(["model/canvas", "model/images", "model/sounds"], 
+    function(Canvas, Images, Sounds) {
+        return {
+            canvas: Canvas,
+            images: Images,
+            sounds: Sounds
+        };
+    }); 
