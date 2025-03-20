f139135f72bb3e33475c19628ea646b0726d0863
Alejandro <alejandro.mont.cesp@gmail.com>
2025-03-19 21:23:17 -0400

    Se modifico las variables var a const y let en un for

--- js/controller/action.js
+++ js/controller/action.js
@@ -1,8 +1,8 @@
 define(["model/dependencies"], function(deps) {
-    var getMousePos = function getMousePos(evt) {
+    const getMousePos = function getMousePos(evt) {
         deps.Game.keyboard.use = false;
         deps.Game.mouse.use = true;
-        var rect = deps.Canvas.canvas.getBoundingClientRect();
+        const rect = deps.Canvas.canvas.getBoundingClientRect();
         deps.Game.mouse.pos.x = evt.clientX - rect.left;
         deps.Game.mouse.pos.y = evt.clientY - rect.top;
         deps.Character.ship.player.pos.y = deps.Game.mouse.pos.y;
@@ -11,18 +11,18 @@ define(["model/dependencies"], function(deps) {
         }
     };
 
-    var resize = function resize() {
+    const resize = function resize() {
         deps.Canvas.contextCanvasWidth = window.innerWidth;
         deps.Canvas.contextCanvasHeight = window.innerHeight - 70;
         deps.Canvas.canvasWidth = deps.Canvas.canvas.width;
         deps.Canvas.canvasHeight = deps.Canvas.canvas.height;
-        var canvas = document.getElementById("gameCanvas");
-        var context = canvas.getContext("2d");
+        const canvas = document.getElementById("gameCanvas");
+        const context = canvas.getContext("2d");
         context.canvas.width = window.innerWidth;
         context.canvas.height = window.innerHeight - 70;
     };
 
-    var mouseClicked = function mouseClicked(down, kb) {
+    const mouseClicked = function mouseClicked(down, kb) {
         deps.Game.keyboard.use = false;
         deps.Game.mouse.use = true;
         switch (deps.Game.screen) {
@@ -62,7 +62,7 @@ define(["model/dependencies"], function(deps) {
         }
     };
 
-    var moveShip = function moveShip() {
+    const moveShip = function moveShip() {
         if (deps.Game.keyboard.use) {
             if (deps.Game.keyboard.up && deps.Character.ship.player.pos.y >= 10) {
                 deps.Character.ship.player.pos.y -= 10;
@@ -72,11 +72,11 @@ define(["model/dependencies"], function(deps) {
         }
     };
 
-    var gameOverButtonCheck = function gameOverButtonCheck() {
-        var part1 = deps.Canvas.canvasWidth / 4;
-        var part2 = deps.Canvas.canvasHeight / 4;
-        var mouseX = deps.Game.mouse.pos.x;
-        var mouseY = deps.Game.mouse.pos.y;
+    const gameOverButtonCheck = function gameOverButtonCheck() {
+        const part1 = deps.Canvas.canvasWidth / 4;
+        const part2 = deps.Canvas.canvasHeight / 4;
+        const mouseX = deps.Game.mouse.pos.x;
+        const mouseY = deps.Game.mouse.pos.y;
         if (mouseX >= part1 * 1.2 && mouseX <= part1 * 1.95 && mouseY >= part2 && mouseY <= part2 * 1.7) {
             if (!deps.Game.muteSFX) {
                 deps.Sounds.select.play();
@@ -93,11 +93,11 @@ define(["model/dependencies"], function(deps) {
         }
     };
 
-    var optionsButtonCheck = function optionsButtonCheck() {
-        var part1 = deps.Canvas.canvasWidth / 4;
-        var part2 = deps.Canvas.canvasHeight / 4;
-        var mouseX = deps.Game.mouse.pos.x;
-        var mouseY = deps.Game.mouse.pos.y;
+    const optionsButtonCheck = function optionsButtonCheck() {
+        const part1 = deps.Canvas.canvasWidth / 4;
+        const part2 = deps.Canvas.canvasHeight / 4;
+        const mouseX = deps.Game.mouse.pos.x;
+        const mouseY = deps.Game.mouse.pos.y;
         if (mouseX >= part1 * 1.2 && mouseX <= part1 * 1.95 && mouseY >= part2 && mouseY <= part2 * 1.7) {
             if (!deps.Game.muteSFX) {
                 deps.Sounds.select.play();
@@ -131,11 +131,11 @@ define(["model/dependencies"], function(deps) {
         }
     };
 
-    var statsButtonCheck = function statsButtonCheck() {
-        var part1 = deps.Canvas.canvasWidth / 4;
-        var part2 = deps.Canvas.canvasHeight / 4;
-        var mouseX = deps.Game.mouse.pos.x;
-        var mouseY = deps.Game.mouse.pos.y;
+    const statsButtonCheck = function statsButtonCheck() {
+        const part1 = deps.Canvas.canvasWidth / 4;
+        const part2 = deps.Canvas.canvasHeight / 4;
+        const mouseX = deps.Game.mouse.pos.x;
+        const mouseY = deps.Game.mouse.pos.y;
         if (mouseX >= part1 * 2.1 && mouseX <= part1 * 2.85 && mouseY >= part2 && mouseY <= part2 * 1.7) {
             if (!deps.Game.muteSFX) {
                 deps.Sounds.select.play();
@@ -150,11 +150,11 @@ define(["model/dependencies"], function(deps) {
         }
     };
 
-    var mainMenuButtonCheck = function mainMenuButtonCheck() {
-        var part1 = deps.Canvas.canvasWidth / 4;
-        var part2 = deps.Canvas.canvasHeight / 4;
-        var mouseX = deps.Game.mouse.pos.x;
-        var mouseY = deps.Game.mouse.pos.y;
+    const mainMenuButtonCheck = function mainMenuButtonCheck() {
+        const part1 = deps.Canvas.canvasWidth / 4;
+        const part2 = deps.Canvas.canvasHeight / 4;
+        const mouseX = deps.Game.mouse.pos.x;
+        const mouseY = deps.Game.mouse.pos.y;
         if (mouseX >= part1 * 1.2 && mouseX <= part1 * 1.95 && mouseY >= part2 && mouseY <= part2 * 1.7) {
             if (!deps.Game.muteSFX) {
                 deps.Sounds.select.play();
@@ -184,11 +184,11 @@ define(["model/dependencies"], function(deps) {
         }
     };
 
-    var enemyShoot = function enemyShoot(x, y, damage) {
+    const enemyShoot = function enemyShoot(x, y, damage) {
         if (!deps.Game.muteSFX) {
             deps.Sounds.laser2.play();
         }
-        var bullet = {
+        const bullet = {
             x: x,
             y: y + 52,
             damage: damage,
@@ -198,25 +198,25 @@ define(["model/dependencies"], function(deps) {
         deps.InPlay.enemyBullets.push(bullet);
     };
 
-    var playerShoot = function playerShoot() {
+    const playerShoot = function playerShoot() {
         if (deps.Game.screen !== "game") return;
 
-        var upgrade = deps.Character.ship.player.upgrade;
+        const upgrade = deps.Character.ship.player.upgrade;
         if (!deps.Game.muteSFX) {
             deps.Sounds.laser1.play();
         }
 
-        var environmentalFactors = {
+        const environmentalFactors = {
             temperature: Math.random() * 30 + 10,
             humidity: Math.random() * 100,
             pressure: Math.random() * 50 + 950
         };
 
-        var calculateBulletAdjustment = function(factors) {
+        const calculateBulletAdjustment = function(factors) {
             return (factors.temperature / 100) + (factors.humidity / 1000) - (factors.pressure / 10000);
         };
 
-        var bullet = {
+        const bullet = {
             x: deps.Character.ship.player.pos.x + 60,
             y: deps.Character.ship.player.pos.y - 4,
             alive: true
@@ -225,8 +225,8 @@ define(["model/dependencies"], function(deps) {
         bullet.damage = deps.Character.ship.player.damage * (1 + calculateBulletAdjustment(environmentalFactors));
         bullet.type = deps.Images.blueLaser1;
 
-        var bulletTrajectory = [];
-        for (var i = 0; i < 5; i++) {
+        const bulletTrajectory = [];
+        for (let i = 0; i < 5; i++) {
             bulletTrajectory.push({
                 x: bullet.x + (i * 20),
                 y: bullet.y - (i * 0.5)
@@ -242,7 +242,7 @@ define(["model/dependencies"], function(deps) {
         deps.InPlay.playerBullets.push(bullet);
     };
 
-    var resetVariables = function resetVariables() {
+    const resetVariables = function resetVariables() {
         deps.Game.gameOver = false;
         deps.Game.timer = 0;
         deps.Game.level = 1;
@@ -259,7 +259,7 @@ define(["model/dependencies"], function(deps) {
         deps.Character.ship.player.fireRate = 3;
     };
 
-    var Action = {
+    const Action = {
         moveShip: moveShip,
         shooting: null,
         mouseClicked: mouseClicked,
