bb752b1e4a4a3559d019a168a2f4c3529efdbc56
grovergonzalez <grover.gonzalez@ucb.edu.bo>
2025-03-19 22:50:20 -0400

    refactorizando la funcion checkpickup

--- js/controller/gameLogic.js
+++ js/controller/gameLogic.js
@@ -175,31 +175,42 @@ define(["model/game", "model/character", "model/inPlay", "model/canvas", "model/
         InPlay.powerUps.push(pickUp);
     };
 
-    var checkPickUp = function checkPickUp() {
-        var powerUps = InPlay.powerUps;
-        var player = Character.ship.player;
-        var i;
-        for (i = 0; i < powerUps.length; i++) {
-            if (powerUps[i].alive) {
-                if (powerUps[i].x >= player.pos.x && powerUps[i].x <= (player.pos.x + player.width)) {
-                    if (powerUps[i].y >= (player.pos.y - player.height) && powerUps[i].y <= player.pos.y + player.height / 2) {
-                        if (!Game.muteSFX) {
-                            Sounds.powerUp.play();
-                        }
-                        if (powerUps[i].type === "health") {
-                            player.hp += 20;
-                        } else if (powerUps[i].type === "fireRate") {
-                            player.fireRate -= 0.09;
-							GameLogic.fRate = true;
-                        } else if (powerUps[i].type === "damage") {
-                            player.damage += 1;
-                        }
-                        powerUps[i].alive = false;
-                    }
+    const checkPickUp = () => {
+        const powerUps = InPlay.powerUps;
+        const player = Character.ship.player;
+    
+        powerUps.forEach(powerUp => {
+            if (powerUp.alive && isCollidingWithPlayer(powerUp, player)) {
+                if (!Game.muteSFX) {
+                    Sounds.powerUp.play();
                 }
+    
+                applyPowerUp(powerUp, player);
+                powerUp.alive = false;
             }
+        });
+    };
+    
+    const isCollidingWithPlayer = (powerUp, player) => {
+        return powerUp.x >= player.pos.x && powerUp.x <= (player.pos.x + player.width) &&
+               powerUp.y >= (player.pos.y - player.height) && powerUp.y <= player.pos.y + player.height / 2;
+    };
+    
+    const applyPowerUp = (powerUp, player) => {
+        const powerUpActions = {
+            health: () => player.hp += 20,
+            fireRate: () => {
+                player.fireRate -= 0.09;
+                GameLogic.fRate = true;
+            },
+            damage: () => player.damage += 1
+        };
+    
+        if (powerUpActions[powerUp.type]) {
+            powerUpActions[powerUp.type]();
         }
     };
+    
 
     var checkShipCollision = function checkShipCollision() {
         var enemies = InPlay.enemies;
